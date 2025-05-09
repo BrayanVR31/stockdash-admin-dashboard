1e6841b3f3ae7680502c5866c78e3e22ead701d2
Brayan Vazquéz Ramírez <brayan.vazquez.ramirez.2000@gmail.com>
2025-05-09 10:16:20 -0600

    fix(script seed): add two models (sale, purchase)

--- backend/src/factories/DocFactory.ts
+++ backend/src/factories/DocFactory.ts
@@ -3,6 +3,8 @@ import RolSeeder from "@/seeders/RolSeeder";
 import CategorySeeder from "@/seeders/CategorySeeder";
 import SupplierSeeder from "@/seeders/SupplierSeeder";
 import ProductSeeder from "@/seeders/ProductSeeder";
+import SaleSeeder from "@/seeders/SaleSeeder";
+import PurchaseSeeder from "@/seeders/PurchaseSeeder";
 
 abstract class DocFactory {
   public static async run() {
@@ -11,6 +13,8 @@ abstract class DocFactory {
     await SupplierSeeder.exec();
     await ProductSeeder.exec();
     await UserSeeder.exec();
+    await SaleSeeder.exec();
+    await PurchaseSeeder.exec();
   }
 }
 
