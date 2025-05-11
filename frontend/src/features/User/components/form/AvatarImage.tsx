a32482de954f6928ca20952159c79cc8e6870793
Brayan Vazquéz Ramírez <brayan.vazquez.ramirez.2000@gmail.com>
2025-05-11 13:56:11 -0600

    feat(avatar): add avatar feature

--- frontend/src/features/User/components/form/AvatarImage.tsx
+++ frontend/src/features/User/components/form/AvatarImage.tsx
@@ -1,5 +1,18 @@
 import { useFormContext } from "react-hook-form";
-import { Flex, Text, Button, Stack, Avatar } from "@chakra-ui/react";
+import {
+  Dialog,
+  Stack,
+  Avatar,
+  Portal,
+  Button,
+  ButtonGroup,
+  FileUpload,
+  Ico,
+  ButtonGroupn,
+  Box,
+  CloseButton,
+} from "@chakra-ui/react";
+import { FaRegFolderOpen } from "react-icons/fa";
 import { UserInputs } from "@/models/userSchema";
 
 const AvatarImage = () => {
@@ -15,7 +28,46 @@ const AvatarImage = () => {
         <Avatar.Fallback name="User profile" />
         <Avatar.Image src="" />
       </Avatar.Root>
-      <Button colorPalette="blue">Subir imagen</Button>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <Button colorPalette="blue">Subir imagen</Button>
+        </Dialog.Trigger>
+        <Portal>
+          <Dialog.Backdrop />
+          <Dialog.Positioner>
+            <Dialog.Content>
+              <Dialog.Header>
+                <Dialog.Title>Subida de archivos</Dialog.Title>
+              </Dialog.Header>
+              <Dialog.Body>
+                <ButtonGroup w="full" colorPalette="blue">
+                  <FileUpload.Root maxFiles={2}>
+                    <FileUpload.HiddenInput />
+                    <FileUpload.Trigger asChild>
+                      <Button variant="outline">
+                        <FaRegFolderOpen />
+                        Seleccionar archivos
+                      </Button>
+                    </FileUpload.Trigger>
+                    <FileUpload.List showSize clearable />
+                  </FileUpload.Root>
+                </ButtonGroup>
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Dialog.ActionTrigger asChild>
+                  <Button variant="outline">Cancelar</Button>
+                </Dialog.ActionTrigger>
+                <Button colorPalette="blue" variant="subtle">
+                  Adjuntar archivos
+                </Button>
+              </Dialog.Footer>
+              <Dialog.CloseTrigger asChild>
+                <CloseButton size="sm" />
+              </Dialog.CloseTrigger>
+            </Dialog.Content>
+          </Dialog.Positioner>
+        </Portal>
+      </Dialog.Root>
     </Stack>
   );
 };
